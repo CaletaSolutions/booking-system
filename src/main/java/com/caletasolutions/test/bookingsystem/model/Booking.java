f9bfed6ecd6487b33b09898c6b6e7405f324415a
Luciano Matos <lucianommatos@gmail.com>
2024-01-08 09:54:16 +0000

    Removed lombok

--- src/main/java/com/caletasolutions/test/bookingsystem/model/Booking.java
+++ src/main/java/com/caletasolutions/test/bookingsystem/model/Booking.java
@@ -1,13 +1,10 @@
 package com.caletasolutions.test.bookingsystem.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Booking {
 
@@ -22,5 +19,87 @@ public class Booking {
     private String bookingDate;
     private String bookingTime;
     private String bookingStatus;
+
+    public Booking() {
+    }
+
+    public Booking(Long id, String customerName, String customerEmail, String customerPhone, String bookingDetails, String bookingDate, String bookingTime, String bookingStatus) {
+        this.id = id;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhone = customerPhone;
+        this.bookingDetails = bookingDetails;
+        this.bookingDate = bookingDate;
+        this.bookingTime = bookingTime;
+        this.bookingStatus = bookingStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getBookingDetails() {
+        return bookingDetails;
+    }
+
+    public void setBookingDetails(String bookingDetails) {
+        this.bookingDetails = bookingDetails;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(String bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+
+    public String toString() {
+        return "Booking{" + "id=" + id + ", customerName=" + customerName + ", customerEmail=" + customerEmail + ", customerPhone=" + customerPhone + ", bookingDetails=" + bookingDetails + ", bookingDate=" + bookingDate + ", bookingTime=" + bookingTime + ", bookingStatus=" + bookingStatus + '}';
+    }
 
 }
